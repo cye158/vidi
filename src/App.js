5fbd9eb113c2ac1fd205b1a3908a9971724b3459
Clarence Enriquez <cye0105@mail.sfsu.edu>
2019-03-22 02:12:02 -0700

    adding routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React, { Component } from "react";
+import { Route, Redirect, Switch } from "react-router-dom";
 import "./App.css";
 import Movies from "./components/movies";
+import Customers from "./components/customers";
+import Rentals from "./components/rentals";
+import NotFound from "./components/notFound";
 
 class App extends Component {
   render() {
     return (
       <main className="container">
-        <h3>Showing Movies</h3>
-        <Movies />
+        <Switch>
+          <Route path="/movies" component={Movies} />
+          <Route path="/customers" component={Customers} />
+          <Route path="/rentals" component={Rentals} />
+          <Route path="/not-found" component={NotFound} />
+          <Redirect from="/" exact to="/movies" />
+          <Redirect to="/not-found" />
+        </Switch>
       </main>
     );
   }
